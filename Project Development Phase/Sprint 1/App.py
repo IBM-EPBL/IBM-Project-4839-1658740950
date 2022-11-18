a12790567648374188c295da0be0014cfadcb3d7
bala15402 <113238780+bala15402@users.noreply.github.com>
2022-11-18 22:40:15 +0530

    Create App.py
    Co-Authored-By: navin457 <113234561+navin457@users.noreply.github.com>
    Co-Authored-By: sriram6622 <113230277+sriram6622@users.noreply.github.com>
    Co-Authored-By: Anbu987 <113233713+Anbu987@users.noreply.github.com>

---
+++ Project Development Phase/Sprint 1/App.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template, flash,redirect, url_for,request
+app = Flask(__name__)
+app.secret_key="hello"
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+	
+
+@app.route("/about")                                      
+def about():
+	return render_template("about.html")
+
+@app.route("/retail")
+def retail():
+  return render_template('retail.html')
+
+@app.route('/signup',methods=["post","get"])
+def signup():
+  return render_template('signup.html')
+
+@app.route("/login")
+def login():
+  return render_template('login.html')  
+
+
+if __name__=='__main__':
+    app.run(debug=True)
