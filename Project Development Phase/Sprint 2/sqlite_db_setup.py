c75f212dcc9c958267a6266d8fa5f7b474611ee7
bala15402 <113238780+bala15402@users.noreply.github.com>
2022-11-20 01:36:55 +0530

    Update sqlite_db_setup.py

--- Project Development Phase/Sprint 2/sqlite_db_setup.py
+++ Project Development Phase/Sprint 2/sqlite_db_setup.py
@@ -3,6 +3,6 @@ import sqlite3
 conn = sqlite3.connect('student_database.db')
 print("Opened database successfully")
 
-conn.execute('CREATE TABLE students ( username TEXT, email TEXT, password TEXT )')
+conn.execute('CREATE TABLE student ( username TEXT, email TEXT, password TEXT )')
 print("Table created successfully")
 conn.close()
